9ae2f455e8d8c00971f6ccc37b631bef15234518
Nikita <kaska0093@inbox.ru>
2023-11-16 12:20:03 -0800

    Initial Commit

---
+++ MVP_002/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP_002
+//
+//  Created by Nikita Shestakov on 16.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
